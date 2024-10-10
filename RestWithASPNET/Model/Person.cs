2727f575de45eb8b7b26d53b8c63b385836a0d9a
paulilio <paulilio.ferreira@gmail.com>
2024-10-10 11:14:49 -0300

    Include Rest Http Verbs For Person

---
+++ RestWithASPNET/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+
+namespace RestWithASPNET.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
---
+++ RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,71 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count; //Siluma Id
+
+        public Person Create(Person person)
+        {
+            //Acesso base
+            //Persistencia
+
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            //Logica deleção
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++) {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Last Name" + i,
+                Address = "Some Address" + i,
+                Gender = (i % 2 == 0) ? "Male" : "Female"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Eduard",
+                LastName = "Castello",
+                Address = "Uberaba - São Paulo - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            //Base de Dados
+            //Update
+            return person;
+        }
+    }
+}
